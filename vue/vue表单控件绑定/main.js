24c07d4492c17fcfe1aa2052b5db6430cfd28b50
461863712 <461863712@qq.com>
2017-08-23 17:48:13 +0800

    页面

---
+++ vue/vue表单控件绑定/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by DELL- on 2017/8/23.
+ */
+(function () {
+
+    new Vue({
+        el:"#app",
+        data:{
+            message:"Hello World",
+            checked:"true",
+            checkedNames:[],
+            picked:"",
+            selected:"",
+            msg:"",
+            age:"",
+            msga:""
+        }
+    })
+
+})();
